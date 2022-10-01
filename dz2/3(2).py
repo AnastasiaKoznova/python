198a221dcd6d5434c5fe1a44a7229cfb968c302d
Koznova_Anastasia <muromlanka@yandex.ru>
2022-10-02 00:52:57 +0300

    65

--- dz2/3(2).py
+++ dz2/3(2).py
@@ -1,6 +1,6 @@
 #Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
 N=int(input("Введите число")) #int для введения целого числа
-def array(number):
+def array(n):
     list=[]
     for i in range (1, number+1): 
         list.append((1+1/n)**n)
