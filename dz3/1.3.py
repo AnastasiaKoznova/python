6d1f00f26af915d7ef1ecdf4b724a0a876e1777c
Koznova_Anastasia <muromlanka@yandex.ru>
2022-10-07 00:00:32 +0300

    3

--- dz3/1.3.py
+++ dz3/1.3.py
@@ -1,5 +1,10 @@
 #Задайте рандомно список из чисел размером N, где N число с клавиатуры. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+from curses.ascii import isdigit
+from ipaddress import summarize_address_range
+
+
 N= [i for i in range(int(input('Введите число:')))]
 list=N[1::2] #делаем срез (1-с элемента с индексом 1, шаг 2)
 print(sum(list))
+
 
